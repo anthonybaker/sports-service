65a6a6ef22f0ac4219ab557706f6c56d6eaf9461
Anthony Baker <abakerp@gmail.com>
2016-06-24 18:25:43 +0100

    removing dotenv ref

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Botkit = require('botkit');
 var http = require('http');
 var url = require('url');
 var request = require('request');
-require('dotenv').config({silent: true});
+//require('dotenv').config({silent: true});
 
 if (!process.env.page_token) {
     console.log('Error: Specify page_token in environment');
